8ce313d0a45e8dea81a293f281780e4242677e50
Bekbol <113009915+askarbekov1@users.noreply.github.com>
2024-12-15 16:40:48 +0600

    Add files via upload

---
+++ War Card Game/War_Card_Game__App.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_Game__Less_4App.swift
+//  War Card Game. Less-4
+//
+//  Created by User on 12/25/22.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_Game__Less_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
